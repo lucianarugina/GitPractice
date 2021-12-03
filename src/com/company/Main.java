b9a5a78db6854a62edb5754274c66dccedba50e7
Luciana <lucianarugina@gmail.com>
2021-12-03 19:43:03 +0200

    Added content and comment in Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,29 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Tema 1 (Fizz Buzz)(predata pe mail):
+
+package com.company;
+import java.util.Scanner;
+        public class Main {
+            public static void main(String[] args) {
+//Tema 1
+                Scanner input = new Scanner(System.in);
+                System.out.print("Input a number: ");
+                int number = input.nextInt();
+                if (number % 3 == 0) {
+                    System.out.print("Fizz");
+                }
+                if (number % 5 == 0) {
+                    System.out.print(" Buzz");
+                }
+                if (!(number % 3 == 0 || number % 5 == 0)) {
+                    System.out.print(number);
+                }
+            }
+        }
     }
 }
